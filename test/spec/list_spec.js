ffcdde03ce1569ea260184b8a502a4f185ef7460
cclamb <cclamb@cs.unm.edu>
2011-10-08 14:38:26 -0600

    added new spec

---
+++ test/spec/list_spec.js
@@ -0,0 +1,9 @@
+var gitit = require('../../lib/gitit');
+
+describe('item', function() {
+  it('should be creatable without args');
+  it('should be creatable with a name argument');
+  it('should be creatable with an array of items');
+  it('should be creatable with a name and item array');
+  it('should be copyable');
+});
